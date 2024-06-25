a166e1baf0b26f593145bc1d298aeb0a4e30fba7
tanyuushaa <rudenko2005w@gmail.com>
2024-06-25 18:15:04 +0300

    Update SimpleTest.java

--- src/test/java/com/example/SimpleTest.java
+++ src/test/java/com/example/SimpleTest.java
@@ -5,4 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class SimpleTest {
+
+    @Test
+    public void testAddition(){
+        int a = 4;
+        int b = 6;
+        Assumptions.assumeTrue(a+b>8, "Sum of numbers not more than 8");
+        int sum = a+b;
+        assertThat(sum).isEqualTo(10);
+    }
 }
