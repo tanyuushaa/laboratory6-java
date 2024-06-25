e54686faac3cfd9e540275451669a629d4579e78
tanyuushaa <rudenko2005w@gmail.com>
2024-06-25 17:29:20 +0300

    Create SimpleTest.java

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,8 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assumptions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SimpleTest {
+}
